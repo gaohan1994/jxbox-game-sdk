1a8a50d8c3c182eddf063d74a92ad819f0c3ada2
Harper Gao <harper.gao@ringcentral.com>
2022-08-04 13:18:18 +0800

    Refactor faction model

--- src/model/__test__/faction.spec.ts
+++ src/model/__test__/faction.spec.ts
@@ -1,6 +1,5 @@
 import test from 'ava';
-import { getAllFactions, getFactionById } from '../faction';
-import { Faction } from '../faction/model';
+import { getAllFactions, getFaction } from '../faction';
 import data from '../faction/data.json';
 
 test.beforeEach('create all factions before test faction cases', (it) => {
@@ -10,28 +9,15 @@ test.beforeEach('create all factions before test faction cases', (it) => {
 
 test('test all factions length should equal to data.json', (it) => {
   const { allFactions } = it.context as any;
-  it.is(allFactions.length, data.data.length);
+  it.is(allFactions.length, data.list.length);
 });
 
-test('faction should instanceof Faction class ', (it) => {
-  const { allFactions } = it.context as any;
-  for (let i = 0; i < allFactions.length; i++) {
-    const currentFaction = allFactions[i];
-    it.is(currentFaction instanceof Faction, true);
-
-    const currentFactionData = data.data[i];
-    it.is(currentFaction.id, currentFactionData.id);
-    it.is(currentFaction.name, currentFactionData.name);
-    it.is(currentFaction.description, currentFactionData.description);
-  }
-});
-
-test('faction should same data as data.json ', (it) => {
+test('faction should with correct faction data ', (it) => {
   const { allFactions } = it.context as any;
   for (let i = 0; i < allFactions.length; i++) {
     const currentFaction = allFactions[i];
 
-    const currentFactionData = data.data[i];
+    const currentFactionData = data.list[i];
     it.is(currentFaction.id, currentFactionData.id);
     it.is(currentFaction.name, currentFactionData.name);
     it.is(currentFaction.description, currentFactionData.description);
@@ -39,12 +25,14 @@ test('faction should same data as data.json ', (it) => {
 });
 
 test('should throw error when find faction with wrong id', (it) => {
-  it.throws(() => getFactionById(99), {
+  it.throws(() => getFaction(99), {
     message: 'Unvalid faction id',
   });
 });
 
 test('should find faction by Id', (it) => {
-  const faction = getFactionById(1);
-  it.is(faction instanceof Faction, true);
+  const faction = getFaction(1);
+  it.is(faction.id, 1);
+  it.is(faction.name, '江湖');
+  it.is(faction.description, '');
 });
